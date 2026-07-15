9301fed527a501767ea36d48818cf6365797297a
hirshtom-web <hirshtom@gmail.com>
2026-07-15 19:49:26 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,6 +1,8 @@
+let savedScrollPosition=0;
 function openDeal(page){
     let modal=document.getElementById("dealModal");
     let frame=document.getElementById("dealFrame");
+    savedScrollPosition=window.scrollY;
     frame.src=page;
     document.documentElement.classList.add("modal-open");
     document.body.classList.add("modal-open");
@@ -16,11 +18,15 @@ function closeDeal(){
     document.body.classList.remove("modal-open");
     frame.src="";
     history.pushState(null,"",window.location.pathname);
+    setTimeout(function(){
+        window.scrollTo(0,savedScrollPosition);
+    },50);
 }
 window.addEventListener("load",function(){
     let params=new URLSearchParams(window.location.search);
     let deal=params.get("deal");
     if(deal){
+        savedScrollPosition=window.scrollY;
         openDeal(deal+".html");
     }
 });
