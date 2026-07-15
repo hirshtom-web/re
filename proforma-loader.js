39b5e8899ef279aaeb59464d86a397f029c11849
hirshtom-web <hirshtom@gmail.com>
2026-07-15 13:17:28 +0300

    Refactor fetch call for proforma.html

--- proforma-loader.js
+++ proforma-loader.js
@@ -1,36 +1,10 @@
-fetch("components/proforma.html")
-
-.then(response => {
-
-    if(!response.ok){
-
-        throw new Error("Proforma file not found");
-
-    }
-
-    return response.text();
-
-})
-
-
-.then(data => {
-
+fetch("./components/proforma.html")
+.then(response => response.text())
+.then(html => {
 
     document
     .getElementById("proforma-container")
-    .innerHTML = data;
+    .innerHTML = html;
 
-
-    loadPropertyData();
-
-    setupTabs();
-
-
-})
-
-
-.catch(error => {
-
-    console.error(error);
 
 });
