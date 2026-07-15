eb0e731896fb385fc3e4799ddf98e837f31ed53e
hirshtom-web <hirshtom@gmail.com>
2026-07-15 13:10:28 +0300

    Update proforma-loader.js

--- proforma-loader.js
+++ proforma-loader.js
@@ -1,12 +1,36 @@
 fetch("components/proforma.html")
-.then(response => response.text())
+
+.then(response => {
+
+    if(!response.ok){
+
+        throw new Error("Proforma file not found");
+
+    }
+
+    return response.text();
+
+})
+
+
 .then(data => {
 
-document.getElementById(
-"proforma-container"
-).innerHTML = data;
+
+    document
+    .getElementById("proforma-container")
+    .innerHTML = data;
 
 
-loadProforma();
+    loadPropertyData();
+
+    setupTabs();
+
+
+})
+
+
+.catch(error => {
+
+    console.error(error);
 
 });
