80782e7e132363a311b4f739016e57449e4bf5ce
hirshtom-web <hirshtom@gmail.com>
2026-07-17 14:39:36 +0300

    Add property data for Starbucks investment

---
+++ property-data.js
@@ -0,0 +1,23 @@
+const properties = {
+    "starbucks-altamonte": {
+
+        title: "Starbucks",
+
+        subtitle: "Brand-New 15-Year Corporate Guaranteed Retail Investment",
+
+        address: "803 N State Road 434<br>Altamonte Springs, Florida 32714",
+
+        purchasePrice: 5314286,
+
+        noi: 279000,
+
+        capRate: 5.25,
+
+        leaseTerm: "15 Years",
+
+        leaseStructure: "NN Corporate",
+
+        image: "https://static.wixstatic.com/media/1799ca_5751d816c9174b74a1724ff2a399cd80~mv2.png"
+
+    }
+};
