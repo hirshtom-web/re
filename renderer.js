692371aaad6a91e404555db4e0850d05c9120501
hirshtom-web <hirshtom@gmail.com>
2026-07-17 14:39:12 +0300

    Create renderer.js

---
+++ renderer.js
@@ -0,0 +1,22 @@
+const property = properties["starbucks-altamonte"];
+
+document.getElementById("property-title").textContent =
+    property.title;
+
+document.getElementById("property-subtitle").textContent =
+    property.subtitle;
+
+document.getElementById("property-address").innerHTML =
+    property.address;
+
+document.getElementById("purchase-price").textContent =
+    "$" + property.purchasePrice.toLocaleString();
+
+document.getElementById("annual-noi").textContent =
+    "$" + property.noi.toLocaleString();
+
+document.getElementById("cap-rate").textContent =
+    property.capRate + "%";
+
+document.getElementById("lease-term").textContent =
+    property.leaseTerm;
