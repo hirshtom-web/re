7b74ca84b93b157c59afa83b9e6a0bba43d63711
hirshtom-web <hirshtom@gmail.com>
2026-07-20 16:53:05 +0300

    Add Azure Residences data to residences.js

---
+++ js/residences.js
@@ -0,0 +1,87 @@
+const residences = {
+
+    "azure-residences": {
+
+        title:
+        "Azure Residences",
+
+        subtitle:
+        "Luxury waterfront living in Miami Beach",
+
+        location:
+        "Miami Beach, Florida",
+
+        heroImage:
+        "https://static.wixstatic.com/media/1799ca_82f8553239c6465c9143f4d15d26f40d~mv2.jpg",
+
+
+        price:
+        "From $2.45M",
+
+
+        bedrooms:
+        "2 - 5 Bedrooms",
+
+
+        type:
+        "Luxury Condominium",
+
+
+        developer:
+        "Premium Developer",
+
+
+        completion:
+        "2028",
+
+
+        highlights:[
+
+            "Oceanfront residences",
+
+            "Private amenities",
+
+            "Flexible ownership options",
+
+            "High-demand location"
+
+        ],
+
+
+        overview:
+
+        `
+        Azure Residences offers a rare opportunity
+        to own a luxury residence in one of Florida's
+        most desirable coastal destinations.
+        `,
+
+
+        amenities:[
+
+            "Infinity pool",
+
+            "Fitness center",
+
+            "Spa",
+
+            "Private lounge",
+
+            "Valet service"
+
+        ],
+
+
+        gallery:[
+
+            "image1.jpg",
+
+            "image2.jpg",
+
+            "image3.jpg"
+
+        ]
+
+    }
+
+};
