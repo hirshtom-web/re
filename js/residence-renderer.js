152d5d372baf051240918d95117debebe219ca04
hirshtom-web <hirshtom@gmail.com>
2026-07-21 01:19:11 +0300

    Update residence-renderer.js

--- js/residence-renderer.js
+++ js/residence-renderer.js
@@ -268,3 +268,15 @@ slider.addEventListener("scroll",()=>{
 
 
 }
+
+document.querySelectorAll(".read-more").forEach(button => {
+    button.addEventListener("click", () => {
+        const text = button.previousElementSibling;
+
+        text.classList.toggle("expanded");
+
+        button.textContent = text.classList.contains("expanded")
+            ? "Read less"
+            : "Read more";
+    });
+});
