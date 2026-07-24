a419dab1da0087242aa8c70996107fb69c504f94
hirshtom-web <hirshtom@gmail.com>
2026-07-24 19:31:59 +0300

    Create propertyGrid.js

---
+++ data/propertyGrid.js
@@ -0,0 +1,262 @@
+window.propertyGrid = [
+
+{
+id:"casa-bella-bb-italia",
+title:"Casa Bella by B&B Italia",
+location:"Art District",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"tal-aventura",
+title:"TAL Aventura",
+location:"Aventura",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"rivage-bal-harbour",
+title:"Rivage Bal Harbour",
+location:"Bal Harbour",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"alana-bay-harbor",
+title:"Alana Bay Harbor Islands",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"indian-creek-residences",
+title:"Indian Creek Residences and Yacht Club",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"la-baia-bay-harbor",
+title:"La Baia Bay Harbor",
+location:"Bay Harbor Islands",
+price:"From $1M+",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"la-mare-residences",
+title:"La Maré Residences",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"onda-residences",
+title:"Onda Residences",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"origin-residences",
+title:"Origin Residences",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"the-well-bay-harbor",
+title:"THE WELL Bay Harbor Islands",
+location:"Bay Harbor Islands",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"alina-residences",
+title:"Alina Residences",
+location:"Boca Raton",
+price:"From $1M+",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"mandarin-oriental-boca",
+title:"Mandarin Oriental Boca",
+location:"Boca Raton",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"2200-brickell",
+title:"2200 Brickell",
+location:"Brickell",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"888-brickell",
+title:"888 Brickell",
+location:"Brickell",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"baccarat-residences-miami",
+title:"Baccarat Residences Miami",
+location:"Brickell",
+price:"From $2M+",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"cipriani-residences-miami",
+title:"Cipriani Residences Miami",
+location:"Brickell",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"domus-brickell",
+title:"Domus Brickell Park",
+location:"Brickell",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"lofty-brickell",
+title:"Lofty Brickell",
+location:"Brickell",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"aston-martin-residences",
+title:"Aston Martin Residences",
+location:"Downtown Miami",
+price:"From $1M+",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+},
+
+
+{
+id:"waldorf-astoria-miami",
+title:"Waldorf Astoria Residences Miami",
+location:"Downtown Miami",
+price:"Request Pricing",
+images:[
+"image1.jpg",
+"image2.jpg",
+"image3.jpg"
+]
+}
+
+];
