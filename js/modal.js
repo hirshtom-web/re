ae223720d794cb4b28aa3103988c06130c439d3a
hirshtom-web <hirshtom@gmail.com>
2026-07-25 00:45:14 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -191,7 +191,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     if(propertyID){
 
         openModal(
-            "residence.html",
+            "property.html",
             propertyID
         );
 
