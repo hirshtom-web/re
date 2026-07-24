cf0fff50466a9cf7fbdde8dba82cee54624cff9f
hirshtom-web <hirshtom@gmail.com>
2026-07-24 22:48:50 +0300

    Update properties-page.js

--- data/properties-page.js
+++ data/properties-page.js
@@ -1,5 +1,71 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+function filterProperties(){
 
-    renderPropertiesGrid();
+
+const search =
+document
+.getElementById("property-search")
+.value
+.toLowerCase();
+
+
+
+const location =
+document
+.getElementById("location-filter")
+.value;
+
+
+
+let filtered =
+window.residences.filter(property=>{
+
+
+const searchMatch =
+
+property.title.toLowerCase().includes(search)
+
+||
+
+property.location.toLowerCase().includes(search);
+
+
+
+const locationMatch =
+
+!location
+
+||
+
+property.location.includes(location);
+
+
+
+return searchMatch && locationMatch;
+
 
 });
+
+
+
+renderPropertiesGrid(filtered);
+
+
+}
+
+
+
+document
+.getElementById("property-search")
+.addEventListener(
+"input",
+filterProperties
+);
+
+
+
+document
+.getElementById("location-filter")
+.addEventListener(
+"change",
+filterProperties
+);
