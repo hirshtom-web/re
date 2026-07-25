6129c2e67d2e92c22eb14744ff127ea11bfb988f
hirshtom-web <hirshtom@gmail.com>
2026-07-25 14:02:52 +0300

    Update properties-map.js

--- js/properties-map.js
+++ js/properties-map.js
@@ -1,10 +1,27 @@
+let map;
+
+
+function initMap(){
+
+    map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            center:{
+                lat:31.7683,
+                lng:35.2137
+            },
+            zoom:12
+        }
+    );
+
+
+    loadPropertiesMap();
+
+}
+
+
+
 function loadPropertiesMap(){
-
-    if(typeof google === "undefined"){
-        console.warn("Google Maps not loaded");
-        return;
-    }
-
 
     if(!window.properties){
         console.warn("No properties found");
@@ -30,9 +47,3 @@ function loadPropertiesMap(){
     });
 
 }
-
-
-document.addEventListener(
-"DOMContentLoaded",
-loadPropertiesMap
-);
