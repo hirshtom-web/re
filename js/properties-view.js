e1c539c5beac3850db902209d128aa6223c48d81
hirshtom-web <hirshtom@gmail.com>
2026-07-25 12:07:24 +0300

    Create properties-view.js

---
+++ js/properties-view.js
@@ -0,0 +1,39 @@
+const gridBtn =
+document.getElementById("grid-view-btn");
+
+
+const mapBtn =
+document.getElementById("map-view-btn");
+
+
+const layout =
+document.querySelector(".properties-layout");
+
+
+
+gridBtn.onclick=()=>{
+
+
+layout.classList.remove("map-only");
+
+gridBtn.classList.add("active");
+
+mapBtn.classList.remove("active");
+
+
+};
+
+
+
+mapBtn.onclick=()=>{
+
+
+layout.classList.add("map-only");
+
+
+mapBtn.classList.add("active");
+
+gridBtn.classList.remove("active");
+
+
+};
