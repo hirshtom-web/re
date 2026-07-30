a7db59dd88e9c2e73c8c0deaa28b263f51339830
hirshtom-web <hirshtom@gmail.com>
2026-07-30 15:50:44 +0300

    Update property-renderer.js

--- js/property-renderer.js
+++ js/property-renderer.js
@@ -191,7 +191,7 @@ const images = (data.images || []).filter(img =>
 
 
 const main =
-document.getElementById("gallery-main");
+document.getElementById("property-main-image");
 
 
 const desktopGrid =
