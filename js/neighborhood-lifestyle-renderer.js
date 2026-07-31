a3d08be035d3b2af3328a7de4f54563134d7d33e
hirshtom-web <hirshtom@gmail.com>
2026-07-31 17:27:19 +0300

    Update neighborhood-lifestyle-renderer.js

--- js/neighborhood-lifestyle-renderer.js
+++ js/neighborhood-lifestyle-renderer.js
@@ -46,11 +46,9 @@ function renderNeighborhoodLifestyle(data){
                 ${item.icon || "location_on"}
             </span>
 
-
             <h3>
                 ${item.title}
             </h3>
-
 
             <p>
                 ${item.text}
@@ -63,84 +61,6 @@ function renderNeighborhoodLifestyle(data){
 
 
     });
-
-
-}
-
-            card.innerHTML=`
-
-                <span class="material-symbols-outlined">
-                    ${item.icon || "location_on"}
-                </span>
-
-
-                <h3>
-                    ${item.title}
-                </h3>
-
-
-                <p>
-                    ${item.text}
-                </p>
-
-            `;
-
-
-            lifestyleGrid.appendChild(card);
-
-
-        });
-
-    }
-
-
-
-
-    const slider =
-    document.getElementById("lifestyle-slider");
-
-
-
-    if(slider && data.lifestyleExperiences){
-
-
-        slider.innerHTML="";
-
-
-        data.lifestyleExperiences.forEach(item=>{
-
-
-            const slide =
-            document.createElement("div");
-
-
-            slide.className =
-            "lifestyle-slide";
-
-
-            slide.innerHTML=`
-
-                <img src="${item.image}"
-                alt="${item.title}">
-
-
-                <div class="slide-overlay">
-
-                    <h3>${item.title}</h3>
-
-                    <p>${item.text}</p>
-
-                </div>
-
-            `;
-
-
-            slider.appendChild(slide);
-
-
-        });
-
-    }
 
 
 }
