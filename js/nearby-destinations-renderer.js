317e3298043891d53b0b5736c9a07bac513536f5
hirshtom-web <hirshtom@gmail.com>
2026-07-31 14:52:03 +0300

    Create nearby-destinations-renderer.js

---
+++ js/nearby-destinations-renderer.js
@@ -0,0 +1,59 @@
+// ======================================
+// NEARBY DESTINATIONS RENDERER
+// ======================================
+
+function renderNearby(data){
+
+    const nearbyGrid =
+        document.getElementById("nearby-grid");
+
+
+    console.log("NEARBY DATA:", data?.nearby);
+
+
+    if(!nearbyGrid || !data?.nearby){
+        return;
+    }
+
+
+    nearbyGrid.innerHTML = "";
+
+
+    data.nearby.forEach(item=>{
+
+        const card =
+            document.createElement("div");
+
+
+        card.className =
+            "nearby-card";
+
+
+        card.innerHTML = `
+
+            <span class="material-symbols-outlined">
+                ${item.icon || "location_on"}
+            </span>
+
+
+            <div>
+
+                <strong>
+                    ${item.title}
+                </strong>
+
+
+                <p>
+                    ${item.distance} away
+                </p>
+
+            </div>
+
+        `;
+
+
+        nearbyGrid.appendChild(card);
+
+    });
+
+}
