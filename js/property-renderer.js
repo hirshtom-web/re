6528ff8b89dbbfbbffc93d9644b521ee0f8e3c45
hirshtom-web <hirshtom@gmail.com>
2026-07-31 14:30:55 +0300

    Update property-renderer.js

--- js/property-renderer.js
+++ js/property-renderer.js
@@ -55,59 +55,6 @@ function renderProperty(data){
 
 
 
-    /* =========================
-       RESIDENCE TABLE
-    ========================= */
-
-    const tbody =
-        document.getElementById("residence-table-body");
-
-    if(tbody && data.residences){
-
-        tbody.innerHTML = "";
-
-        data.residences.forEach(item => {
-
-            const row =
-                document.createElement("tr");
-
-            row.innerHTML = `
-
-                <td>${item.name}</td>
-
-                <td>${item.bedrooms}</td>
-
-                <td>${item.bathrooms}</td>
-
-                <td>${item.interior}</td>
-
-                <td class="plan-cell">
-
-                    <button
-                        class="plan-link"
-                        data-plan="${item.layout}"
-                        data-title="${item.name} Floor Plan">
-
-                        <img
-                            src="${item.preview || item.layout}"
-                            class="plan-thumb"
-                            alt="${item.name} Floor Plan">
-
-                    </button>
-
-                </td>
-
-            `;
-
-            tbody.appendChild(row);
-
-        });
-
-    }
-
-
-
-
 /* =========================
    FACTS
 ========================= */
