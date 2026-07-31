67b1e6a2d371c4f0d6fe4565e0e406e8dd77b13f
hirshtom-web <hirshtom@gmail.com>
2026-07-31 15:44:00 +0300

    Create lifestyle-experiences-renderer.js

---
+++ js/lifestyle-experiences-renderer.js
@@ -0,0 +1,62 @@
+function renderLifestyleExperiences(data){
+
+
+    const lifestyleSlider =
+    document.getElementById("lifestyle-slider");
+
+
+
+    if(!lifestyleSlider || !data.lifestyleExperiences){
+        return;
+    }
+
+
+
+    lifestyleSlider.innerHTML = "";
+
+
+
+    data.lifestyleExperiences.forEach(item=>{
+
+
+        const slide =
+        document.createElement("div");
+
+
+        slide.className =
+        "lifestyle-slide";
+
+
+        slide.innerHTML = `
+
+            <img 
+                src="${item.image}"
+                alt="${item.title}"
+            >
+
+
+            <div class="slide-overlay">
+
+
+                <h3>
+                    ${item.title}
+                </h3>
+
+
+                <p>
+                    ${item.text}
+                </p>
+
+
+            </div>
+
+        `;
+
+
+        lifestyleSlider.appendChild(slide);
+
+
+    });
+
+
+}
