228da332560c9ebf9a0eaebe50623ba4cde42dcb
hirshtom-web <hirshtom@gmail.com>
2026-08-03 15:48:30 +0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -22,15 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
     ===================== */
 
     const toggle = document.querySelector(".mobile-toggle");
-    const nav = document.querySelector(".nav-links");
+const nav = document.querySelector(".nav-links");
+
+if(toggle && nav){
 
     toggle.addEventListener("click", () => {
 
         nav.classList.toggle("active");
-
         toggle.classList.toggle("open");
 
     });
+
+}
 
 
 
